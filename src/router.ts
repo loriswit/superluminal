68ad64dac9abb9435694bb299b4e94553aff477d
Loris Witschard <loris.wit@gmail.com>
2020-11-30 20:52:00 +0100

    Add page titles

--- src/router.ts
+++ src/router.ts
@@ -6,14 +6,23 @@ import Resources from "./views/Resources.vue"
 import Submissions from "./views/Submissions.vue"
 import Verifiers from "./views/Verifiers.vue"
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     {path: "/", component: Home},
-    {path: "/map", component: Maps},
-    {path: "/guide", component: Guide},
-    {path: "/resources", component: Resources},
-    {path: "/submissions", component: Submissions},
-    {path: "/verifiers", component: Verifiers},
+    {path: "/map", component: Maps, meta: {title: "Maps"}},
+    {path: "/guide", component: Guide, meta: {title: "Guide"}},
+    {path: "/resources", component: Resources, meta: {title: "Resources"}},
+    {path: "/submissions", component: Submissions, meta: {title: "Submissions"}},
+    {path: "/verifiers", component: Verifiers, meta: {title: "Verifiers"}},
   ]
 })
+
+router.afterEach((to) => {
+  if(!to.meta.title)
+    document.title = "Superluminal"
+  else
+    document.title = to.meta.title + " - Superluminal"
+})
+
+export default router
